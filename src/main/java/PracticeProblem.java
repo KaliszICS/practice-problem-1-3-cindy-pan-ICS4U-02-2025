620f977438263d3e01b7935956ffd02db8a98b43
cindy-pan-ics4u1-02-25 <cindy.pan@student.tdsb.on.ca>
2025-03-06 14:36:19 +0000

    Update

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,39 @@
+import java.io.*;
+
 public class PracticeProblem {
 
-	public static void main(String args[]) {
+	public static String readFile(String filename) {
+		FileReader fiRe = null;
+		int i;
+			char o;
+			String out = "";
+		try{
+			 fiRe = new FileReader(filename);
+			
+			while ((i = fiRe.read())!=-1){
+				o = ((char)i);
+				out = out + String.valueOf(o);
+				out = out.replaceAll("[\\\r\\\n]+","");
+			} 
+		}
+		catch (Exception e){
 
+		}
+		
+		finally {
+			try {
+				if (fiRe!=null){
+					fiRe.close();
+				}
+			}
+			catch (IOException e){
+
+			}
+		}
+		return out;
 	}
+}
 
 	
 
-}
+
